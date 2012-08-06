a3b984292fd8433d0ac132b7a729b95cab044d97
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-08-06 12:07:48 +0200

    Links to shared setuphandler functions

--- seantis/dir/roadworks/setuphandlers.py
+++ seantis/dir/roadworks/setuphandlers.py
@@ -1,23 +1,10 @@
-def get_fti(context, typename):
-    types = context.getSite().portal_types
-
-    if typename in types:
-        return types[typename]
-    else:
-        return None
-
-def add_behavior(fti, behavior):
-    if fti:
-        behaviors = list(fti.behaviors)
-        if not behavior in behaviors:
-            behaviors.append(behavior)
-            fti.behaviors = tuple(behaviors)
+from seantis.dir.base.setuphandlers import get_fti, add_behavior
 
 def installBehavior(context):
     """Registers behaviors for seantis.dir.base.item."""
 
-    fti = get_fti(context, 'seantis.dir.base.item')
+    fti = get_fti('seantis.dir.base.item')
     add_behavior(fti, 'seantis.dir.roadworks.item.IRoadworksDirectoryItem')
 
-    fti = get_fti(context, 'seantis.dir.base.directory')
+    fti = get_fti('seantis.dir.base.directory')
     add_behavior(fti, 'seantis.dir.roadworks.directory.IRoadworksDirectory')
