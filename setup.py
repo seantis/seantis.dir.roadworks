f4fec1b51cd27c38756e678a9b5da0793c6d78d7
Denis Krienbühl <denis.krienbuehl@gmail.com>
2013-04-17 15:59:14 +0200

    Release 1.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.2a1'
+version = '1.3'
 
 setup(name='seantis.dir.roadworks',
       version=version,
@@ -30,7 +30,7 @@ setup(name='seantis.dir.roadworks',
           'collective.autopermission',
           'collective.testcaselayer',
           'collective.dexteritytextindexer',
-          'seantis.dir.base'
+          'seantis.dir.base>=1.3'
       ],
       entry_points="""
       [z3c.autoinclude.plugin]
