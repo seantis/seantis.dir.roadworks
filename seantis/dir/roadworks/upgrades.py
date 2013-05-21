c8b54353d5f7254329d4c609dd9cab32b4a6277f
Denis Krienbühl <denis.krienbuehl@gmail.com>
2013-05-21 10:16:05 +0200

    Adds index for 'works' to display it in the directory result list

--- seantis/dir/roadworks/upgrades.py
+++ seantis/dir/roadworks/upgrades.py
@@ -1,8 +1,16 @@
+from Products.CMFCore.utils import getToolByName
+
 from seantis.dir.roadworks.item import IRoadworksDirectoryItem
 from seantis.dir.base.upgrades import add_behavior_to_item
 
 
 def upgrade_to_1000(context):
+
+    # add the new index first as the next step also does a reindex of all items
+    setup = getToolByName(context, 'portal_setup')
+    setup.runImportStepFromProfile(
+        'profile-seantis.dir.roadworks:default', 'catalog'
+    )
 
     add_behavior_to_item(
         context, 'seantis.dir.roadworks', IRoadworksDirectoryItem
